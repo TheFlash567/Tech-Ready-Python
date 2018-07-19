d3310c3a40303e8288945c684533426b4af30a84
TheFlash567 <41442505+TheFlash567@users.noreply.github.com>
2018-07-19 13:56:12 -0400

    Update Level 1.py

--- Level 1.py
+++ Level 1.py
@@ -3,7 +3,11 @@
 import os
 import random
 import pygame
+import math
 
+
+
+death = 0
 # Class for the orange dude
 class Player(object):
     
@@ -17,7 +21,8 @@ class Player(object):
             self.move_single_axis(dx, 0)
         if dy != 0:
             self.move_single_axis(0, dy)
-    
+
+        
     def move_single_axis(self, dx, dy):
         
         # Move the rect
@@ -45,18 +50,25 @@ class Wall(object):
 
 class Enemy(object):
 
-    def __init__(self, pos, movetype):
+    def __init__(self, pos, movetype, ang=0):
+        self.ang = ang
+        self.r = 40
         self.movetype = movetype
         self.pos = pos
         self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
         self.rect.center = (pos[0], pos[1])
+        self.startx = self.rect.centerx
+        self.starty = self.rect.centery
         enemies.append(self)
 
     def move(self, dx, dy):
         self.rect.centerx += dx
         self.rect.centery += dy
-
-
+    
+    def spin(self):
+        self.rect.centerx = math.cos(self.ang) * self.r + self.startx
+        self.rect.centery = math.sin(self.ang) * self.r + self.starty
+        self.ang += 0.05
 
 
 # Initialise pygame
@@ -64,7 +76,7 @@ os.environ["SDL_VIDEO_CENTERED"] = "1"
 pygame.init()
 
 # Set up the display
-pygame.display.set_caption("Get to the green zone!")
+pygame.display.set_caption("The World's Easiest Game!")
 screen = pygame.display.set_mode((688, 235))
 
 clock = pygame.time.Clock()
@@ -73,26 +85,25 @@ enemies = []
 player = Player() # Create the player
 
 count = 0 
-speed = 5
-reps = 10
+speed = 3
+reps = 35
 
 # Holds the level layout in a list of strings.
 level = [
 "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
-"W                   U                     W",
-"W                                         W",
-"W                                         W",
-"W                                         W",
 "W                                         W",
 "W                   U                     W",
 "W                                         W",
-"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW   H  W",
+"W                                         W",
+"W                                         W",
+"W                   U                     W",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW H    W",
 "WE                                        W",
-"W            C               C            W",
 "W                                         W",
-"W          C C C           C C C          W",
 "W                                         W",
-"W            C               C            W",
+"W          C                   C          W",
+"W                                         W",
+"W                                         W",
 "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
 ]
 
@@ -104,6 +115,9 @@ for row in level:
             Wall((x, y))
         if col == "C":
             Enemy((x, y), 'r')
+            Enemy((x, y), 'r', math.pi/2)
+            Enemy((x, y), 'r', math.pi)
+            Enemy((x, y), 'r', 3*math.pi/2)
         if col == "U":
             Enemy((x, y), 'u')
         if col == "H":
@@ -146,9 +160,12 @@ while running:
             enemy.move(0, speed)
         elif enemy.movetype == 'h':
             enemy.move(speed, 0)
+        elif enemy.movetype == 'r':
+            enemy.spin()
         if player.rect.colliderect(enemy.rect):
             player.rect = pygame.Rect(32, 32, 16, 16)
-            print("You died!")
+            death += 1
+            print(f"You died {death} time(s)!")
     
     count += 1
     
@@ -157,9 +174,9 @@ while running:
         count = 0
     
     # Draw the scene
-    screen.fill((0, 0, 0))
+    screen.fill((255, 255, 255))
     for wall in walls:
-        pygame.draw.rect(screen, (255, 255, 255), wall.rect)
+        pygame.draw.rect(screen, (0, 0, 0), wall.rect)
     for enemy in enemies:
         pygame.draw.circle(screen, (0, 0, 255), enemy.rect.center, 8)
     pygame.draw.rect(screen, (0, 255, 50), end_rect)
